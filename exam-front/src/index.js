cba601c17f0d3c05a8949603e021d13c8f773ef9
Kurmanbekov Ernur <erazavr85@gmail.com>
2020-07-18 17:37:06 +0600

    Закончил писать контрольную

--- exam-front/src/index.js
+++ exam-front/src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {Provider} from 'react-redux';
+import {ConnectedRouter} from "connected-react-router";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import store, {history} from "./store/configureStore";
+import App from './App';
+
+import 'react-toastify/dist/ReactToastify.css';
+
+const app = (
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <App/>
+        </ConnectedRouter>
+    </Provider>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(app, document.getElementById('root'));
