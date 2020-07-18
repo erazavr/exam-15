a730e997e6f88871f4b3d47f104cb6d8c51b5ff1
Kurmanbekov Ernur <erazavr85@gmail.com>
2020-07-18 18:09:49 +0600

    Написал шаги для тестов

---
+++ exam-test/step_definitions/steps.js
@@ -0,0 +1,41 @@
+const { I } = inject();
+
+Given('я нахожусь на странице логина', () => {
+    I.amOnPage('/login')
+});
+Given('я нахожусь на главной странице', () => {
+    I.amOnPage('/');
+    I.wait(3)
+});
+
+When('я заполняю поля формы:', table => {
+    const tableData = table.parse().rawData;
+
+    tableData.forEach(row => {
+        if (row[0] === 'image') {
+            I.attachFile(row[0], row[1]);
+        }
+        if (row[0] === 'qof' || row[0] === 'sq' || row[0] === 'interior') {
+            I.selectOption(row[0], row[1])
+        }
+        else {
+            I.fillField(row[0], row[1])
+        }
+    });
+
+
+});
+
+When('нажимаю на кнопку {string}', name => {
+    I.click(name);
+    I.wait(3)
+
+});
+
+
+
+Given('перехажу на главную страницу', () => {
+  I.amOnPage('/');
+  I.wait(3)
+});
+
